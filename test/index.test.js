7c3a75b8d2284c3fe43b3a702b00ceab7270167f
KauanAmorim <kauanamorim88@gmail.com>
2022-08-19 17:25:30 -0300

    refactoring test and add test about getFileLinks result

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,19 @@
+const path = require('path');
 const getFileLinks = require('../extract-links')
 
-test('getFileLinks need tobe a function', () => {
-    expect(typeof getFileLinks).toBe('function')
+const arrayLinkResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('getFileLinks::', () => {
+    it('should be a function', () => {
+        expect(typeof getFileLinks).toBe('function')
+    })
+    it('should return an array with a link result', async () => {
+        const filePath = path.resolve('./test/files/text.md')
+        const result = await getFileLinks(filePath)
+        expect(result).toEqual(arrayLinkResult)
+    })
 })
