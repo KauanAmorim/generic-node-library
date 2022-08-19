72483d051fc99506e2552952cf1f4f21403559fa
KauanAmorim <kauanamorim88@gmail.com>
2022-08-18 22:11:59 -0300

    change portuguese to english

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const chalk = require('chalk');
 const fs = require('fs')
 
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'));
+    throw new Error(chalk.red(erro.code, 'None file found'));
 }
 
-async function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(filePath) {
     const encoding = 'utf-8';
     try {
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+        const text = await fs.promises.readFile(filePath, encoding)
+        console.log(chalk.green(text))
     } catch (erro) {
         trataErro(erro);
     } finally {
-        console.log(chalk.yellow('operação concluída'));
+        console.log(chalk.yellow('operation concluded'));
     }
 }
 
